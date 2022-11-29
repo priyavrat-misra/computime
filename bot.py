ce8643d212e3b11aa16e9d2a5dc0baf442840a50
Priyavrat Misra <priyavratmisra123@gmail.com>
2022-11-29 08:05:21 +0530

    add /help command

--- bot.py
+++ bot.py
@@ -5,17 +5,19 @@ import configparser as cfg
 class chatbot():
     def __init__(self, config):
         self.token = self.get_token_from_file(config)
-        self.base_url = f"https://api.telegram.org/bot{self.token}/"
+        self.base_url = f'https://api.telegram.org/bot{self.token}/'
 
     def get_updates(self, offset=None):
-        url = self.base_url + "getUpdates?timeout=100"
+        url = self.base_url + 'getUpdates?timeout=100'
         if offset:
-            url += f"&offset={offset + 1}"
+            url += f'&offset={offset + 1}'
         r = requests.get(url)
         return json.loads(r.content)
 
     def send_message(self, msg, chat_id):
-        url = self.base_url + f"sendMessage?chat_id={chat_id}&text={msg}&parse_mode=HTML&disable_web_page_preview=True"
+        url = (f'{self.base_url}'
+               f'sendMessage?chat_id={chat_id}&text={msg}'
+               f'&parse_mode=HTML&disable_web_page_preview=True')
         if msg is not None:
             requests.get(url)
 
--- server.py
+++ server.py
@@ -1,15 +1,17 @@
 import time
 from bot import chatbot
 
-bot = chatbot("./config.cfg")
+bot = chatbot('./config.cfg')
 
 def make_reply(msg):
-    usage = 'Send messages in this format:\u000a'\
-            '<code>hrs:mins:secs speed</code>\u000a'\
-            '<i>E.g., 3:15:53 2.75</i>\u000a\u000a'
+    usage = ('Send messages in this format:\u000a'
+            '<code>hrs:mins:secs speed</code>\u000a'
+            '<i>E.g., 3:15:53 2.75</i>\u000a\u000a')
 
-    if msg == "/start":
-        return usage + 'by @prv_t • <a href="https://github.com/priyavrat-misra/computime">Source code</a>'
+    if msg == "/start" or msg == "/help":
+        return (f'{usage}'
+                'Bot by @prv_t • '
+                '<a href="https://github.com/priyavrat-misra/computime">Source code</a>')
     try:
         dur, speed = msg.split()
         speed = float(speed)
@@ -17,31 +19,27 @@ def make_reply(msg):
         t1 = t2 = (h % 24) * 3600 + m * 60 + s
         t1 //= speed
         t2 -= t1
-        msg = f"<b>{dur}</b> @ <b>{speed}x</b> will take <b>{computime(t1)}</b>"
-        msg += f" and save <b>{computime(t2)}</b>"
-        return msg
+        return (f'<b>{dur}</b> @ <b>{speed}x</b>'
+               f' will take <b>{time.strftime("%H:%M:%S", time.gmtime(t1))}</b>'
+               f' and save <b>{time.strftime("%H:%M:%S", time.gmtime(t2))}</b>')
     except:
-        return 'Invalid input.\u000a\u000a' + usage + \
-                'Bug? Please report it <a href="https://github.com/priyavrat-misra/computime/issues">here</a>.'
-
-def computime(secs):
-    '''
-    converts time in seconds to time in hours, minutes and seconds
-    '''
-    return time.strftime("%H:%M:%S", time.gmtime(secs))
+        return ('Invalid input.\u000a\u000a'
+                f'{usage}'
+                'Bug? Report it '
+                '<a href="https://github.com/priyavrat-misra/computime/issues">here</a>.')
 
 update_id = None
 while True:
     updates = bot.get_updates(offset=update_id)
-    updates = updates["result"]
+    updates = updates['result']
     if updates:
         for item in updates:
-            update_id = item["update_id"]
+            update_id = item['update_id']
             try:
-                message = item["message"]["text"]
+                message = item['message']['text']
             except:
                 message = None
-            from_ = item["message"]["from"]["id"]
+            from_ = item['message']['from']['id']
             reply = make_reply(message)
             bot.send_message(reply, from_)
 
